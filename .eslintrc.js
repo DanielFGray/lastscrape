9bd9800d182e1ff8641bd05f98c7401c4693c292
DanielFGray <DanielFGray@gmail.com>
2017-05-05 21:35:43 -0500

    :hatching_chick:

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: 'airbnb-base',
+  rules: {
+    'semi': [ 'error', 'never' ],
+    'no-unexpected-multiline': [ 'error' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'no-console': 'off',
+    'no-unused-vars': 'warn',
+    'no-confusing-arrow': 'warn',
+  },
+}
---
+++ lastscrape.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const { Observable } = require('rxjs')
+const { get } = require('superagent')
+const commander = require('commander')
+const { memoize, merge } = require('ramda')
+
+commander
+  .option('-u, --user [string]', 'Last.fm username')
+  .parse(process.argv)
+
+const query = {
+  api_key: 'e38cc7822bd7476fe4083e36ee69748e',
+  format: 'json',
+  limit: 50,
+}
+
+const lastfm = opts =>
+  Observable.from(get('http://ws.audioscrobbler.com/2.0/')
+    .query(merge(query, opts)))
+    .map(x => x.body)
+
+const requestUserTracks = ({ page }) => {
+  process.stderr.write(`fetching page ${page}\r`)
+  return lastfm({
+    method: 'user.getrecenttracks',
+    user: commander.user,
+    page,
+  })
+}
+
+const genreOf = memoize(([ artist, album ]) =>
+  lastfm({
+    method: 'album.gettoptags',
+    artist,
+    album,
+  }))
+
+const requestAllTracks = opts =>
+  Observable.defer(() => requestUserTracks(opts)
+    .flatMap((x) => {
+      const current = Number(x.recenttracks['@attr'].page)
+      const total = Number(x.recenttracks['@attr'].totalPages)
+      const items$ = Observable.of(x)
+      const next$ =
+        1 + current <= total
+        ? requestAllTracks({ page: 1 + current }) :
+        Observable.empty()
+      return Observable.concat(items$, next$)
+    }))
+
+const track$ = requestAllTracks({ page: 1 })
+  .map(x => x.recenttracks)
+  .flatMap(x => x.track)
+  .map(x => ({
+    date: x.date ? x.date.uts : '',
+    artist: x.artist['#text'],
+    name: x.name,
+    album: x.album['#text'],
+  }))
+
+  .flatMap(x =>
+    genreOf([ x.artist, x.album ])
+      .map(t => t.toptags.tag.slice(0, 3).map(e => e.name))
+      .map(t => merge(x, { genres: t.join(',') })))
+
+  .flatMap(x =>
+    x.album === '[unknown]' || x.album === ''
+    ? lastfm({ method: 'track.getinfo', artist: x.artist, album: x.album })
+        .map(t => merge(x, { album: t.track.album.title }))
+    : Observable.of(x))
+
+const file = fs.createWriteStream('rx_stream.csv')
+
+const write$ = track$
+  .map(x => `${Object.values(x).join('\t')}\n`)
+  .do(x => process.stdout.write(x))
+  .do(x => file.write(x))
+
+// Observable.merge(write$)
+  .subscribe()
